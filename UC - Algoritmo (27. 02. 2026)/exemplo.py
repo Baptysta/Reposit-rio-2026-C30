75adca34b917be281ac3f0d69914967c389fc154
Baptysta <joaobaptysta043@gmail.com>
2026-02-27 16:08:01 -0300

    Add files via upload

---
+++ UC - Algoritmo (27. 02. 2026)/exemplo.py
@@ -0,0 +1,12 @@
+#altura = float(input("Digite sua altura"))
+#altura = altura * 100
+#print(f"SUa altura é de {altura} cm")
+
+nome = "Bapo pressão"
+idade = 16
+
+texto = "Meu nome é {} e tenho {} anos".format(nome, idade)
+texto = "Meu nome é {n} e tenho {i} anos".format(n=nome, i=idade)
+
+print(texto)
+
