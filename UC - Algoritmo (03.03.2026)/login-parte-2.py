7b7b486f90ab763644bb7d937d5b8b261e79262d
Baptysta <joaobaptysta043@gmail.com>
2026-03-03 14:40:48 -0300

    Add files via upload

---
+++ UC - Algoritmo (03.03.2026)/login-parte-2.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome:")
+senha = input("Digite sua senha:")
+
+while senha == "1234":
+    print(f"Olá,{nome}")
+
---
+++ UC - Algoritmo (03.03.2026)/login.py
@@ -0,0 +1,11 @@
+usuario = input("Digite seu login:")
+senha = input("Digite sua senha:")
+
+if usuario ==": procopio" and senha=="12345":
+    print("Acesso liberado")
+
+elif usuario=="paiva" and senha=="54321":
+    print("Acesso liberado")
+
+else:
+    print("Acesso negado")
