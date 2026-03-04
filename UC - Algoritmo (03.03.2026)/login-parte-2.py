ca92708a6b2a616f0a498ecca9e3760c40409f68
Baptysta <joaobaptysta043@gmail.com>
2026-03-03 23:38:59 -0300

    Update login-parte-2.py

--- UC - Algoritmo (03.03.2026)/login-parte-2.py
+++ UC - Algoritmo (03.03.2026)/login-parte-2.py
@@ -1,6 +1,19 @@
-nome = input("Digite seu nome:")
-senha = input("Digite sua senha:")
+senha = "123456"
+tentativa = 3
+nome = input("Digite seu nome: ")
 
-while senha == "1234":
-    print(f"Olá,{nome}")
+while tentativa > 0:
+    senha_usuario = input("Digite sua senha: ")
+
+if senha_usuario == senha:
+    print(f"Olá,{nome}, seja bem-vindo ao nosso banco!")
+else:
+    tentativa -= 1
+    if tentativa == 2:
+        print("Sua senha está incorreta, você possui mais 2 tentativas.")
+    elif tentativa == 1:
+        print("Sua senha está incorreta, você possui apenas 1 tentativa.")
+    else:
+        print("Sua senha foi bloqueada!")
+
 
