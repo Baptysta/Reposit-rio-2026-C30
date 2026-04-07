22ac9fd84e9dab9ea9be69f42c57197aeb6f82fd
Baptysta <joaobaptysta043@gmail.com>
2026-04-07 13:52:27 -0300

    Add files via upload

---
+++ UC - Algoritmo - OBI (07.04.2026)/OBI.py
@@ -0,0 +1,14 @@
+P = int(input("Digite a quantidade de pães"))
+D = int(input("Digite a quantidade de pães doces"))
+B = int(input("Digite a quantidade de bólos"))
+
+total = P * 1 + D * 2 + B * 3
+
+if total >= 150:
+    print("B")
+elif total >= 120:
+    print("D")
+elif total >= 100:
+    print("B")
+else:
+    print("N")
